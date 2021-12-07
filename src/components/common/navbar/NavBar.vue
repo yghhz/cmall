87e745de507ad487ee47ecd4e83ffdf1ef9ea784
yghhz <793202503@qq.com>
2021-12-07 22:17:49 +0800

    提交近期代码

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="navbar">
+        <div class="left">
+            <slot name="left"></slot>
+        </div>
+        <div class="center">
+            <slot name="center"></slot>
+        </div>
+         <div class="right">
+            <slot name="right"></slot>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'NarBar'
+}
+</script>
+<style scoped>
+    .navbar {
+        display: flex;
+        line-height: 44px;
+        height: 44px;
+        box-shadow: 0px -1px 1px rgba(100,100,100,.2);
+        text-align: center;
+        
+    }
+    .left,.right {
+        width: 60px;
+    }
+    .center {
+        flex: 1;
+    }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,45 @@
 <template>
-  <h2>首页</h2>
+<div id="home">
+  <nav-bar class="home-nav">
+    <div slot="center">购物街</div>
+  </nav-bar>
+  <home-swiper :banners="banners"></home-swiper>
+  <home-recommends :recommends="recommends"></home-recommends>
+</div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
+import HomeSwiper from './childComps/HomeSwiper'
+import HomeRecommends from './childComps/HomeRecommends'
+
+
+import {getMultiData}  from 'network/home'
   export default {
-    name: "Home"
+    name: "Home",
+    data () {
+      return {
+        recommends:[],
+        banners:[]
+      }
+    },
+    components: {
+      NavBar,
+      HomeSwiper,
+      HomeRecommends
+    },
+    created () {
+      getMultiData().then(res =>{
+         this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+      })
+    }
   }
 </script>
 
 <style scoped>
-
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
